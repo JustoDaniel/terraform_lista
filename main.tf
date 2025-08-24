8192cf84450a381ad09ec66676c391581edae87d
Justo Daniel <carlosajdaniel@gmail.com>
2025-08-24 16:22:43 -0300

    Ajustando. Depois de muito tempo verifiquei que o problema era com o usuario. criei outro e funcionou.

--- main.tf
+++ main.tf
@@ -1,14 +1,30 @@
+
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "Justo01" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+}
+
 variable "user_names" {
     description = "Create IAM users testando"
     type = list(string)
     default = [ "Peter","Chris","Stewie" ]  
 }
-
-resource "aws_instance" "Justo01" {
-    ami     = "ami-05803413c51f242b7"
-    instance_type  = "t2.micro"
-}
-
 
 
 resource "aws_iam_user" "quiz_experts_user" {
--- variable.tf
+++ variable.tf
@@ -1,16 +1,17 @@
 variable "AWS_ACCESS_KEY" {
     type = string
     #default = "AKIASMSIZOF42P2VUDSZ"
-    default = "AKIAVS5BAG5RIVAPPDHJ"
-    
+    # default = "AKIAVS5BAG5RIVAPPDHJ"
+    default = "AKIAVS5BAG5RI2KZTSFY"
 }
 
 variable "AWS_SECRET_KEY" {
   # default = "T8bMil7TFrH/jAUIWzG8P7InDd7M6rxW2/8Xuvts"
-  default = "hlYiMLWk/s8tHepdIRoF34m3eqkHZQH+Q+nR7qdK"
+  # default = "hlYiMLWk/s8tHepdIRoF34m3eqkHZQH+Q+nR7qdK"
+  default = "fDZPvwUflpa671q1tgT6zDNjtONXhJmNbdBZafF4"
   
 }
 
 variable "AWS_REGION" {
-default = "us-east-2"
+default = "us-east-1"
 }

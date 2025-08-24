7c2bb59c1534f62f834decfa7e776466c659be28
Justo Daniel <carlosajdaniel@gmail.com>
2025-08-24 15:27:00 -0300

    trocando a chave

--- variable.tf
+++ variable.tf
@@ -1,11 +1,14 @@
 variable "AWS_ACCESS_KEY" {
     type = string
     #default = "AKIASMSIZOF42P2VUDSZ"
-    default = "AKIAVS5BAG5RMO6NYR2T"
+    default = "AKIAVS5BAG5RIVAPPDHJ"
+    
 }
 
 variable "AWS_SECRET_KEY" {
-  default = "T8bMil7TFrH/jAUIWzG8P7InDd7M6rxW2/8Xuvts"
+  # default = "T8bMil7TFrH/jAUIWzG8P7InDd7M6rxW2/8Xuvts"
+  default = "hlYiMLWk/s8tHepdIRoF34m3eqkHZQH+Q+nR7qdK"
+  
 }
 
 variable "AWS_REGION" {

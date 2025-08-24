0d62779740f62e8d64907365f8dbafcf02c80228
Justo Daniel <carlosajdaniel@gmail.com>
2025-08-24 15:23:43 -0300

    teste

--- main.tf
+++ main.tf
@@ -4,6 +4,13 @@ variable "user_names" {
     default = [ "Peter","Chris","Stewie" ]  
 }
 
+resource "aws_instance" "Justo01" {
+    ami     = "ami-05803413c51f242b7"
+    instance_type  = "t2.micro"
+}
+
+
+
 resource "aws_iam_user" "quiz_experts_user" {
     count = length(var.user_names)
     name = var.user_names [count.index]

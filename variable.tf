106716cce994efa670360d298f042623bf4e2d4a
Justo Daniel <35306471+JustoDaniel@users.noreply.github.com>
2025-09-02 17:22:15 -0300

    Update variable.tf
    PRecisei commitar pq tava envolvendo risco de segurança ter essas chaves expostas.

--- variable.tf
+++ variable.tf
@@ -1,11 +1,11 @@
 variable "AWS_ACCESS_KEY" {
     type = string
     #default = "AKIASMSIZOF42P2VUDSZ"
-    default = "AKIAVS5BAG5RMO6NYR2T"
+    #default = "AKIAVS5BAG5RMO6NYR2T"
 }
 
 variable "AWS_SECRET_KEY" {
-  default = "T8bMil7TFrH/jAUIWzG8P7InDd7M6rxW2/8Xuvts"
+  #default = "T8bMil7TFrH/jAUIWzG8P7InDd7M6rxW2/8Xuvts"
 }
 
 variable "AWS_REGION" {
